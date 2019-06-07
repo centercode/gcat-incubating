edde2b0c9030f5a83dff9c499dfe16b43b8b3f3b
yx91490 <yx91490@126.com>
2019-06-07 17:36:19 +0800

    LineParser inner variables

--- src/main/java/io/gcat/parser/CMSParNewParser.java
+++ src/main/java/io/gcat/parser/CMSParNewParser.java
@@ -56,22 +56,21 @@ public class CMSParNewParser implements Parser {
     }
 
     private void parseLineInternal(String line) throws LineParseException {
-        LineParser parser = LineParser.INSTANCE.reset();
-        int c = parser.parseTimestamp(line);
-        c = parser.parseBootTime(line, c);
-        String rest = line.substring(c);
-        if (!CMS && rest.startsWith("[GC (Allocation Failure) ")) {
-            c = parser.parseNewRegion(line, c);
-            c = parser.parseHeap(line, c);
-            parser.parseGcTime(line, c);
+        LineParser parser = LineParser.INSTANCE.reset(line);
+        parser.parseTimestamp();
+        parser.parseBootTime();
+        if (!CMS && parser.restStartWith("[GC (Allocation Failure) ")) {
+            parser.parseNewRegion();
+            parser.parseHeap();
+            parser.parseGcTime();
             GCInfo gcInfo = parser.getGCInfo();
             gcInfo.setType(GCInfo.GCType.GC);
             gcInfo.setRegion(GCInfo.GCRegion.New);
             list.add(gcInfo);
-        } else if (rest.startsWith("[GC (CMS Initial Mark)")) {
+        } else if (parser.restStartWith("[GC (CMS Initial Mark)")) {
             CMS = true;
             CMSStartTimestamp = parser.getTimestamp();
-        } else if (rest.startsWith("[CMS-concurrent-reset:")) {
+        } else if (parser.restStartWith("[CMS-concurrent-reset:")) {
             CMS = false;
             GCInfo gcInfo = parser.getGCInfo();
             gcInfo.setType(GCInfo.GCType.CMS);
@@ -138,31 +137,35 @@ public class CMSParNewParser implements Parser {
 
         INSTANCE;
 
-        private static final Logger logger = LoggerFactory.getLogger(LineParser.class);
+        int cursor;
+
+        private String line;
 
         private GCInfo gcInfo = new GCInfo();
 
-        private int parseTimestamp(String line) throws LineParseException {
+        private void parseTimestamp() throws LineParseException {
             int s = "2019-05-05T15:52:07.063+0800".length();
             try {
                 long timestamp = DateUtil.parse(line.substring(0, s));
                 gcInfo.setTimestamp(timestamp);
-                return s + 2; //skip ": "
+                cursor = s + 2; //skip ": "
             } catch (Exception e) {
                 throw new LineParseException();
             }
         }
 
-        private int parseBootTime(String line, int s) {
+        private void parseBootTime() {
+            int s = cursor;
             int e = line.indexOf(": ", s);
             //parse timestamp
             Float bootTime = Float.valueOf(line.substring(s, e));
             gcInfo.setBootTime((long) (bootTime * 1000));
 
-            return e + 2; //skip ": "
+            cursor = e + 2; //skip ": "
         }
 
-        private int parseNewRegion(String line, int s) {
+        private void parseNewRegion() {
+            int s = cursor;
             s = line.indexOf("[ParNew: ", s) + "[ParNew: ".length();
             Matcher m = changePattern.matcher(line.substring(s, line.length()));
             if (!m.find()) {
@@ -175,10 +178,11 @@ public class CMSParNewParser implements Parser {
             gcInfo.setRegionUsedAfter(regionUsedAfter);
             gcInfo.setRegionSize(regionSize);
 
-            return s + m.group().length() + 2; //skip "] "
+            cursor = s + m.group().length() + 2; //skip "] "
         }
 
-        private int parseHeap(String line, int s) {
+        private void parseHeap() {
+            int s = cursor;
             Matcher m = changePattern.matcher(line.substring(s, line.length()));
             if (!m.find()) {
                 throw new IllegalArgumentException("not found heap change.");
@@ -191,10 +195,11 @@ public class CMSParNewParser implements Parser {
             gcInfo.setHeapUsedAfter(heapUsedAfter);
             gcInfo.setHeapSize(heapSize);
 
-            return s + m.group().length() + 2; //skip "] "
+            cursor = s + m.group().length() + 2; //skip "] "
         }
 
-        private void parseGcTime(String line, int s) {
+        private void parseGcTime() {
+            int s = cursor;
             Matcher m = gcTimePattern.matcher(line.substring(s, line.length()));
             if (m.find()) {
                 Float gcTime = Float.valueOf(m.group(1));
@@ -204,8 +209,14 @@ public class CMSParNewParser implements Parser {
             }
         }
 
-        public LineParser reset() {
-            gcInfo = new GCInfo();
+        private boolean restStartWith(String prefix) {
+            return line.substring(cursor).startsWith(prefix);
+        }
+
+        public LineParser reset(String line) {
+            this.line = line;
+            this.cursor = 0;
+            this.gcInfo = new GCInfo();
             return this;
         }
 
